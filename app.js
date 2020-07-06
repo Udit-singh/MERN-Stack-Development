be779d74bfa601e6d7a01b3cafb4839ade093f60
Udit singh <Udit-singh@users.noreply.github.com>
2020-07-06 21:23:34 +0530

    Assignment 4

--- app.js
+++ app.js
@@ -11,12 +11,14 @@ var authenticate = require('./authenticate');
 
 var config = require('./config');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dishRouter = require('./routes/dishRouter');
-var promoRouter = require('./routes/promoRouter');
-var leaderRouter = require('./routes/leaderRouter');
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+const dishRouter = require('./routes/dishRouter')
+const promoRouter = require('./routes/promoRouter')
+const leaderRouter = require('./routes/leaderRouter')
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
+
 
 const mongoose = require('mongoose');
 const url = config.mongoUrl;
@@ -58,9 +60,10 @@ app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
+app.use('/favorites',favoriteRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 // error handler
